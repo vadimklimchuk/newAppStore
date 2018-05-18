249ecd313a71d03144c212199523d949eaee0d4c
unknown <akella1997@tut.by>
2018-05-18 16:34:13 +0300

    #

--- src/app/auth/auth0-variables.ts
+++ src/app/auth/auth0-variables.ts
@@ -3,10 +3,9 @@ interface AuthConfig {
     domain: string;
     callbackURL: string;
   }
-  
+
   export const AUTH_CONFIG: AuthConfig = {
     clientID: 'GmSsgAyOMWYQ7jPID5cVPJMo1m931NIC',
     domain: 'vadimklimchuk.auth0.com',
-    callbackURL: 'http://localhost:4200/callback'
+    callbackURL: 'https://appstore-8d742.firebaseapp.com'
   };
-  
